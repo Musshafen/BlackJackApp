5c138588a5f52120bbbf7a33674c8f0b3c6ac487
KM Musshafen <kmusshafen@hotmail.com>
2023-02-19 12:20:26 -0500

    Shuffling deck

--- Program.cs
+++ Program.cs
@@ -16,7 +16,14 @@ namespace BlackJackApp
             return 0;
         }
 
-          
+        public string Description()
+        {
+            var newDescriptionString = $"The {Rank} of {Suit}";
+
+            return newDescriptionString;
+
+        }
+
 
 
     }
@@ -30,7 +37,36 @@ namespace BlackJackApp
 
             var deck = new List<Card>();
 
+            var suits = new List<string>() { "Spades", "Clubs", "Diamonds", "Hearts" };
+            var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
 
+
+            foreach (var suit in suits)
+            {
+
+                foreach (var rank in ranks)
+                {
+                    var card = new Card { Suit = suit, Rank = rank };
+                    deck.Add(card);
+
+                }
+
+            }
+
+            var numberOfCards = deck.Count;
+
+            for (var rightIndex = numberOfCards - 1; rightIndex >= 1; rightIndex--)
+            {
+                var randomNumberGenerator = new Random();
+                var leftIndex = randomNumberGenerator.Next(rightIndex);
+
+
+                var leftCard = deck[leftIndex];
+                var rightCard = deck[rightIndex];
+                deck[rightIndex] = leftCard;
+                deck[leftIndex] = rightCard;
+
+            }
 
 
         }
